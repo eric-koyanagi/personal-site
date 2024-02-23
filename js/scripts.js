618869bc2b9e65147f420fbe4c9c6cb043c61a79
eric koyanagi <eric.koyanagi@thetot.com>
2024-02-23 13:08:42 -0800

    Initial redo

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,18 @@
-/*!
-    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-    (function ($) {
-    "use strict"; // Start of use strict
+const progressBar = document.querySelector('.progressBar');
+const pageContainer = document.querySelector('body');
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
-    });
+const scrollProgressBar = () => {
+    const percent = getScrollPercent();
+    progressBar.style.width = percent + '%';
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
+};
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#sideNav",
-    });
-})(jQuery); // End of use strict
+const getScrollPercent = () => {
+    let scrollDistance = -(pageContainer.getBoundingClientRect().top);
+    let progressPercentage = (scrollDistance / (pageContainer.getBoundingClientRect().height - document.documentElement.clientHeight)) * 100;
+
+    let val = Math.floor(progressPercentage);
+    return (val < 0) ? 0 : val;
+}
+
+window.addEventListener('scroll', scrollProgressBar);
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: 'Oswald', 
+        heading: 'Protest Guerrilla, ui-sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
+
